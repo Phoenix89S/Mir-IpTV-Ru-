14f519b6b2b5fe9bd481ece37dacba2159be16b2
Phoenix89S <134923425+Phoenix89S@users.noreply.github.com>
2026-05-02 20:05:57 +0200

    Update find_m3u8_rtrs.py

--- find_m3u8_rtrs.py
+++ find_m3u8_rtrs.py
@@ -9,14 +9,36 @@ WINK_REF = "https://wink.ru/"
 WINK_IP = "95.24.0.1"  # РФ, МТС
 
 # Источники плейлистов
+
 GITHUB_PLAYLISTS = [
+    # --- iptv-org основные ---
     "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru.m3u",
     "https://raw.githubusercontent.com/iptv-org/iptv/master/channels/ru.m3u",
 
-    "http://rafail1982.uz/playlists/LIST2.m3u",
-    "https://raw.githubusercontent.com/Phoenix89S/Mir-IpTV-Ru-/main/code",
-    "http://rafail1982.uz/playlists/DIMONOVICH.m3u",
+    # --- iptv-org расширенные RU ---
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_15plusmg.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_bonustv.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_catcast.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_mylifeisgood.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_ntv.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_rt.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_smotrim.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_televizor24.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_tvbricks.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_tvteleport.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ru_zabava.m3u",
 
+    # --- Rafail1982 ---
+    "http://rafail1982.uz/playlists/LIST2.m3u",
+    "http://rafail1982.uz/playlists/DIMONOVICH.m3u",
+    "http://rafail1982.uz/playlists/TELEKARTA.m3u",
+    "http://rafail1982.uz/playlists/TELECIFRA.m3u",
+
+    # --- Твои проекты ---
+    "https://raw.githubusercontent.com/Phoenix89S/Mir-IpTV-Ru-/main/code",
+    "https://raw.githubusercontent.com/Phoenix89S/Iptv_Ru2026/main/Viju2test.m3u",
+
+    # --- Дополнительные источники ---
     "https://raw.githubusercontent.com/Zet2009/MOJE1/gh-pages/IPTVmir.m3u8",
     "https://raw.githubusercontent.com/smolnp/IPTVru/gh-pages/IPTVstable.m3u8",
 
@@ -26,13 +48,7 @@ GITHUB_PLAYLISTS = [
 
     "https://dtv.plus/rabochiy-iptv-pleylist-tv-kanaly/",
     "https://live.iptv-free.com/iptv/categories/kids.m3u",
-    "https://live.iptv-free.com/iptv/languages/rus.m3u",
-
-    "http://rafail1982.uz/playlists/TELEKARTA.m3u",
-    "http://rafail1982.uz/playlists/TELECIFRA.m3u",
-
-    # 🔥 ТВОЙ НОВЫЙ ИСТОЧНИК (RAW)
-    "https://raw.githubusercontent.com/Phoenix89S/Iptv_Ru2026/main/Viju2test.m3u"
+    "https://live.iptv-free.com/iptv/languages/rus.m3u"
 ]
 
 EPG_SOURCES = "https://epg.one/epg.xml.gz,https://iptvx.one/EPG"
